5dea34cf113b9f7e59b842ed3da33dfd52e7b0d2
carlos.cardona <carlos.cardona@pragma.com.co>
2019-07-23 19:02:43 -0500

    Carga inicial
    Signed-off-by: carlos.cardona <carlos.cardona@pragma.com.co>

---
+++ receive-mq/src/test/java/com/testrabbitmq/receive/AppTest.java
@@ -0,0 +1,14 @@
+package com.testrabbitmq.receive;
+
+import org.junit.Test;
+
+import com.testrabbitmq.receive.Receive;
+
+public class AppTest {
+
+    @Test
+    public void testAppHasAGreeting() {
+        Receive classUnderTest = new Receive();
+        // assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+}
